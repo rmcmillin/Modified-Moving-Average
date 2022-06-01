9c1d26ceb7dc9957f6b4e610a0562210210d1933
rmcmillin <77390891+rmcmillin@users.noreply.github.com>
2022-06-01 11:41:14 -0400

    Initial Release

---
+++ MMA.c
@@ -0,0 +1,160 @@
+/** \file MMA.c
+ * Modified Moving Average c file
+ * 
+ */
+
+#include <avr/io.h>
+#include <stdio.h>
+#include "MMA.h"
+#include "UART.h"
+
+/**
+* Struct definition for moving average
+*/
+
+struct MovingAverage{
+	avgState_t state; /**< USED or UNUSED */
+	uint16_t average; /**< Holds the current moving average */
+	uint32_t sum; /**< Holds the sum of the previous [NUMBER_OF_SAMPLES] samples */
+	uint8_t numberOfSamples; /**< used to prevent low averages when number of samples taken is less than [NUMBER_OF_SAMPLES]  */
+};
+
+/**
+* Struct array to hold all the averages
+*/
+
+static struct MovingAverage mmaList [MAX_AVERAGES];
+
+/**
+*	\brief Register for an ID
+*
+*	\details This function returns an ID that can used to calculate an
+*	average for a particular instance
+*
+*	\return Returns an ID > 0, or negative value on error
+*/
+
+mma_error_t avg_register(uint8_t *avgID){
+	//find the next unused ID and return
+	for (uint8_t i=0; i < MAX_AVERAGES; i++){
+		struct MovingAverage *mma = &mmaList[i];
+		if (mma->state == AVG_UNUSED){
+			//unused average found, initialize and return avgID to be used
+			mma->average = 0;
+			mma->state = AVG_USED;
+			mma->sum = 0;
+			mma->numberOfSamples = 0;
+			*avgID = i;
+			return MMA_SUCCESS;
+		}
+	}
+	return MMA_ERR_ALL_ID_USED;
+}
+
+/**
+* \brief Free an ID
+*
+* \details Sets an ID to be freed so that it can be used elsewhere
+*
+* \param[in] avgID	The ID to be freed
+*
+* \return Returns 0 if successfully freed, returns negative on error
+*/
+
+mma_error_t avg_free(int8_t avgID){
+	if (avgID >= 0 && avgID < MAX_AVERAGES){
+		struct MovingAverage *mma = &mmaList[avgID];
+		if (mma->state != AVG_UNUSED){
+			return MMA_ERR_ID_UNREGISTERED;
+		}
+		mma->average = 0;
+		mma->state = AVG_UNUSED;
+		mma->sum = 0;
+		mma->numberOfSamples = 0;
+	}
+	else{
+		return MMA_ERR_OUT_OF_BOUND;
+	}
+	return MMA_SUCCESS;
+}
+
+/**
+* \brief Move the Average
+*
+* \details This function updates the average with the newest value.  It also detects overflows and low numbers when the number of measurements is lower than NUMBER_OF_SAMPLES
+*
+* \param[in]	avgID		The ID to update
+* \param[in]	newValue	The newest value to add to the moving average
+*
+* \return	Returns 0 on success and a negative value on error
+*/
+
+mma_error_t avg_moveTheAverage(int8_t avgID, uint16_t newValue){
+	if (avgID >= 0 && avgID < MAX_AVERAGES){
+		struct MovingAverage *mma = &mmaList[avgID];
+		
+		//prevent low numbers when number of measurements is less than NUMBER_OF_SAMPLES
+		if (mma->numberOfSamples <= NUMBER_OF_SAMPLES){
+			mma->numberOfSamples++;
+		}
+		
+		//detect overflow
+		if ( (65535 - mma->sum + mma->average) < (newValue) ){//(mma->sum - mma->average + newValue) < mma->sum){
+			return MMA_ERR_OVERFLOW;
+		}
+		
+		//move the average
+		mma->sum -= mma->average;
+		mma->sum += newValue;
+		mma->average = mma->sum / mma->numberOfSamples;
+	}
+	return MMA_ERR_OUT_OF_BOUND;
+}
+
+/**
+* \brief Get average
+*
+* \details get the average from the ID supplied
+*
+* \param[in]	avgID		ID to use
+* \param[out]	*average	updates variable with current moving average
+*
+* \return	returns 0 on success, negative value on error
+*/
+
+mma_error_t avg_getAverage(int8_t avgID, uint16_t *average){
+	if (avgID >= 0 && avgID < MAX_AVERAGES){
+		struct MovingAverage *mma = &mmaList[avgID];
+		if (mma->state != AVG_USED){
+			return MMA_ERR_ID_UNREGISTERED;
+		}
+		*average = mma->average;
+		return MMA_SUCCESS;
+	}
+	return MMA_ERR_OUT_OF_BOUND;
+}
+
+/**
+* \brief Resets Average
+*
+* \details Resets the average and counter for supplied ID
+*
+* \param[in]	avgID	ID to use
+*
+* \return	returns 0 on success, negative value on error
+*/
+
+mma_error_t avg_reset(int8_t avgID){
+	if (avgID >= 0 && avgID < MAX_AVERAGES){
+		struct MovingAverage *mma = &mmaList[avgID];
+		if (mma->state != AVG_USED){
+			return MMA_ERR_ID_UNREGISTERED;
+		}
+		mma->average = 0;
+		mma->numberOfSamples = 0;
+		mma->state = AVG_USED; //we still want to keep it
+		mma->sum = 0;
+		return MMA_SUCCESS;
+	}
+	return MMA_ERR_OUT_OF_BOUND;
+}
---
+++ MMA.h
@@ -0,0 +1,56 @@
+
+/** \file MMA.h
+ * Modified Moving Average Header File
+ * 
+ */
+
+/*! \mainpage Modified Moving Average
+ *
+ * \section Description
+ *
+ * This library provides up to [MAX_AVERAGES] unique modified moving averages. Each average is first registered and then accessed by using an avgID with the provided functions.
+ *
+ * \note This library does not use dynamic memory as it is intended for embedded projects.  All memory is assigned at compile time.  Use [MAX_AVERAGES] to set the amount of memory used.
+ *
+ * \section How To Use
+ *
+ * Update [NUMBER_OF_SAMPLES] to the number of values to be summed for the average.
+ *
+ * Update [MAX_AVERAGES] to the number of MMA averages that are required.
+ *
+ * Register with the module by calling avg_register(uint8_t *avgID).  check the return value of the function before accessing the parameter. On function return, the parameter *avgID holds the ID that can be used with the rest of the functions.
+ *
+ * To add a new value to the average, call avg_moveTheAverage(int8_t avgID, uint16_t newValue), where newValue is the newest number to add to the average.  
+ *
+ * To get the current average, call mma_error_t avg_getAverage(int8_t avgID, uint16_t *average)
+ *
+ * When finished with an average, call mma_error_t avg_free(int8_t avgID) to free the ID so it can be used elsewhere
+ *
+ * To reset the average to 0, call mma_error_t avg_reset(int8_t avgID)
+ *
+ */
+
+//Number of values to use for the moving average
+#define NUMBER_OF_SAMPLES   8
+//Number of averages needed by external modules
+#define MAX_AVERAGES 2 
+
+typedef enum AverageState{
+	AVG_UNUSED,
+	AVG_USED
+}avgState_t;
+
+typedef enum MMA_Error{
+	MMA_ERR_OUT_OF_BOUND = -4,
+	MMA_ERR_ALL_ID_USED,
+	MMA_ERR_ID_UNREGISTERED,
+	MMA_ERR_OVERFLOW,
+	MMA_SUCCESS	
+}mma_error_t;
+
+mma_error_t avg_register(uint8_t *avgID);
+mma_error_t avg_free(int8_t avgID);
+mma_error_t avg_moveTheAverage(int8_t avgID, uint16_t newValue);
+mma_error_t avg_getAverage(int8_t avgID, uint16_t *average);
+mma_error_t avg_reset(int8_t avgID);
+
